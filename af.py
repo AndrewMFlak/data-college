873ffaebc131932718fac3b79c6d376cc99c6e19
AFlak <AndrewMFlak@gmail.com>
2019-05-25 11:40:56 -0400

    some stuff

--- af.py
+++ af.py
@@ -2,13 +2,14 @@ import time
 import datetime
 from bs4 import BeautifulSoup
 import urllib.request,urllib.parse
+#==================PyMongo===================>
 import pymongo
 try:
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 except pymongo.errors.ConnectionFailure as e:
     print(e)
 
-
+#==============SSL Certificate==================>
 import ssl
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -16,4 +17,13 @@ ctx.verify_mode = ssl._create_unverified_context
 
 
 
+
+mydb = myclient["dataSets"]
+mycol = mydb["collegeData"]
+mycol.create_index("_id")
+
 url = 'https://collegescorecard.ed.gov/data/'
+
+import zipfile
+with zipfile.ZipFile(url,"r") as zipedi:
+    zipedi.extractall("./")
